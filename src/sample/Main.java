b4f3e57a4486aa2829e4e9e46ee20a075122e454
supajippy <supajippy@hotmail.com>
2019-02-12 10:02:02 -0500

    Timeline

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,7 +1,9 @@
 package sample;
 
-import javafx.animation.AnimationTimer;
+import javafx.animation.*;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
@@ -9,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
 
@@ -26,29 +29,31 @@ public class Main extends Application {
             balles[i] = new Balle((float) Math.random()*640, (float) Math.random()*360, (float) Math.random()*40 + 30, i, balles);
         }
 
+
         primaryStage.setScene(new Scene(group, 640, 360));
 
-        for (Balle balle : balles) {
-            balle.collide();
-            balle.move();
-            group.getChildren().add(balle.display());
-        }
 
-        new AnimationTimer() {
-            @Override public void handle(long currentNanoTime) {
-                for (int i = 0; i < 50; i++) {
-                    double ran = Math.random() * 640;
-                    graphics.strokeOval(ran, ran, ran, ran);
+
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0),
+                new EventHandler<ActionEvent>() {
+
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        for (Balle balle : balles) {
+                            balle.collide();
+                            balle.move();
+                            group.getChildren().add(balle.display());
+
+                        }
+                    }
                 }
 
-                try {
-                    Thread.sleep(100);
-
-                } catch (InterruptedException e) { }
-            }
-        }.start();
-
+        ), new KeyFrame(Duration.millis(25)));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
