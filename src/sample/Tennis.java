74e32807bdf7da1ee945c997e5f9264d2d668a25
Velmousse <supravelmousse@gmail.com>
2019-02-19 12:31:33 -0500

    Ajout d'un menu de sélection avec le clavier Ajout de la balle de bowling et de la balle de tennis Suppression de la génération des balles au hasard Ajout du placement des balles sur l'écran

---
+++ src/sample/Tennis.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+
+import java.util.ArrayList;
+
+public class Tennis extends Balle {
+
+    public Tennis(float _x, float _y, int _id, ArrayList<Balle> _a, Image image) {
+        x = _x;
+        y = _y;
+        id = _id;
+        autres = _a;
+        diametre = 15;
+        friction = -0.4;
+        pattern = new ImagePattern(image);
+    }
+}
